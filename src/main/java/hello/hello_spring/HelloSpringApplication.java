a8ea112699bf3ef77e53987afe891391ded3ce84
dob-by <yeonhwalim03@gmail.com>
2024-09-30 00:02:42 +0900

    testcase

--- src/main/java/hello/hello_spring/HelloSpringApplication.java
+++ src/main/java/hello/hello_spring/HelloSpringApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloSpringApplication {
 
 	public static void main(String[] args) {
-		System.out.println("test");
 		SpringApplication.run(HelloSpringApplication.class, args);
 	}
 
